68d1fde83c1a9a8aef3118f61a0bf90d50b51322
轮子哥 <jinqun0730@gmail.com>
2025-08-29 14:38:37 +0800

    优化 Demo 工程的演示效果

--- app/src/main/java/com/hjq/device/compat/demo/MainActivity.java
+++ app/src/main/java/com/hjq/device/compat/demo/MainActivity.java
@@ -2,6 +2,7 @@ package com.hjq.device.compat.demo;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
@@ -13,23 +14,26 @@ import com.hjq.bar.TitleBar;
 import com.hjq.device.compat.DeviceBrand;
 import com.hjq.device.compat.DeviceOs;
 
-public class MainActivity extends AppCompatActivity implements OnClickListener {
+public final class MainActivity extends AppCompatActivity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
-        TextView messageView = findViewById(R.id.btn_main_messages);
+        TextView messageView = findViewById(R.id.tv_main_messages);
         StringBuilder stringBuilder = new StringBuilder()
-            .append("\nBrand：" + DeviceBrand.getBrandName())
-            .append("\nOs：" + DeviceOs.getOsName())
-            .append("\nOriginalOsVersionName：" + DeviceOs.getOsVersionName())
-            .append("\nOsVersionName：" + DeviceOs.getOsVersionName())
-            .append("\nOsBigVersionCode：" + DeviceOs.getOsBigVersionCode());
+            .append("\nBrandName: " + DeviceBrand.getBrandName())
+            .append("\nOsName: " + DeviceOs.getOsName())
+            .append("\nOriginalOsVersionName: " + DeviceOs.getOsVersionName())
+            .append("\nOsVersionName: " + DeviceOs.getOsVersionName())
+            .append("\nOsBigVersionCode: " + DeviceOs.getOsBigVersionCode())
+            .append("\nAndroidVersion: Android " + Build.VERSION.RELEASE)
+            .append("\nAndroidApiLevel: " + Build.VERSION.SDK_INT);
         messageView.setText(stringBuilder);
 
         findViewById(R.id.btn_main_about_device).setOnClickListener(this);
+        findViewById(R.id.btn_main_get_system_property).setOnClickListener(this);
         findViewById(R.id.btn_main_android_settings).setOnClickListener(this);
 
         TitleBar titleBar = findViewById(R.id.tb_main_bar);
@@ -49,6 +53,9 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         if (viewId == R.id.btn_main_about_device) {
             // 跳转到关于手机界面
             startActivity(new Intent(Settings.ACTION_DEVICE_INFO_SETTINGS));
+        } else if (viewId == R.id.btn_main_get_system_property) {
+            // 跳转到获取系统属性界面
+            startActivity(new Intent(this, SystemPropertyActivity.class));
         } else if (viewId == R.id.btn_main_android_settings) {
             // 跳转到 Android 设置界面
             startActivity(new Intent(Settings.ACTION_SETTINGS));
